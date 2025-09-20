89a528d742381382d56f7d35ad228199281a2e9a
achref332 <achrefhayouni2021@gmail.com>
2025-09-20 16:39:05 +0100

    Create product.js

---
+++ src/product.js
@@ -0,0 +1,6 @@
+export const product = {
+  name: "Super Widget",
+  price: "$49.99",
+  description: "Un widget fantastique qui rend la vie plus facile.",
+  image: "https://via.placeholder.com/150" 
+};
